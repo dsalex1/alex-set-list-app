6de577365c7b91e8fa5b9fb4ebc99c5c3491d87f
dsalex1 <dsalex1@ymail.com>
2024-09-19 00:13:50 +0200

    made app fullscreen

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 import { transformAssetUrls } from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
-export default ({ mode }:{mode: string}) => {
+export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   return defineConfig({
     plugins: [
@@ -23,6 +23,7 @@ export default ({ mode }:{mode: string}) => {
           scope: process.env.VITE_BASE_URL,
           name: process.env.VITE_APP_NAME,
           short_name: process.env.VITE_APP_SHORT_NAME,
+          display: 'fullscreen',
           description: process.env.VITE_APP_DESCRIPTION,
           theme_color: '#41B883',
           icons: [
